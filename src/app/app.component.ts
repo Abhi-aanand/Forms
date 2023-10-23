489a8adfd8ccc1a2939a1b88adddd752ae1d7a00
Abhi <aanand@argusoft.com>
2023-10-23 15:18:29 +0530

    services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,34 @@ import { User } from './user';
 })
 export class AppComponent {
   topics=['Angular', 'React', 'Vue'];
+  topicHasError=true;
+  submitted=false;
+  errorMsg='';
 
-  userModel = new User('Sabuja' , 'sabuja@gmail.com' , 7033510115 , '' , 'morning' , true);
+  userModel = new User('Sabuja' , 'sabuja@gmail.com' , 7033510115 , 'default' , 'morning' , true);
+  
+  
+  constructor( private _enrollmentservice: EnrollmentService){}
+
+  validateTopic(value: string){
+    if(value === 'default'){
+      this.topicHasError = true;
+    }else{
+      this.topicHasError = false;
+
+    }
+  }
+  onSubmit(userForm: any){
+    console.log(userForm);
+    
+    // this.submitted=true;
+    // this._enrollmentservice.enroll(this.userModel)
+    // .subscribe(
+    //   data=> console.log('Success!',data),
+    //   error=>this.errorMsg=error.statusText
+      
+      
+    // )
+    
+  }
 }
